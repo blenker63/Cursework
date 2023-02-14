a5462736792f4cb063d7543c8d9b5c7b91802c39
vaceslavkameronov <slka@mail.ru>
2023-02-14 23:35:30 +0400

    решение Курсового задания Базовый уровень

--- src/Coursework/Employee.java
+++ src/Coursework/Employee.java
@@ -3,97 +3,84 @@ package Coursework;
 import java.util.Arrays;
 
 public class Employee {
-    private static  Employee[] employees;
     private String surname;
     private String name;
     private String patronymic;
     private int department;
     private int salary;
+    private int counter = 0;
     private int id = 0;
-    private static int counter = 0;
 
-        public Employee() {
-        this.employees = new Employee[10];
-    }
-//    public Employee(String surname, String name, String patronymic, int department, int salary, int id) {
-//        this.surname = surname;
-//        this.name = name;
-//        this.patronymic = patronymic;
-//        this.department = department;
-//        this.salary = salary;
-//        this.id = id;
-//        employees = new Employee[10];
-//    }
 
-//    public Employee() {
-//    }
-
-    public void addEmployee(String surname, String name, String patronymic, int department, int salary, int id) {
+    public Employee(String surname, String name, String patronymic, int department, int salary, int id) {
         this.surname = surname;
         this.name = name;
         this.patronymic = patronymic;
         this.department = department;
         this.salary = salary;
         this.id = id;
-//        if (counter >= employees.length) {
-//            System.out.println("Нельзя добавить сотрудника, закончилось место");
-//        }
-//        Employee newEmployee = new Employee(surname, name, patronymic, department, salary, id); //surname, name, patronymic, department, salary, id
-        Employee newEmployee = new Employee(); //surname, name, patronymic, department, salary, id
-        employees[counter++] = newEmployee;
-        this.id = counter;
-
+    }
+    public Employee[] employees; {
+        employees = new Employee[10];
     }
 
+    public Employee() {
+    }
     public String getSurname() {
         return this.surname;
     }
-
-    public String getName() {
+    public  String getName() {
         return this.name;
     }
-
     public String getPatronymic() {
         return this.patronymic;
     }
-
     public int getDepartment() {
         return this.department;
     }
-
     public int getSalary() {
         return this.salary;
     }
-
     public int getId() {
         return this.id;
     }
-
     public void setDepartment(int department) {
         this.department = department;
     }
-
     public void setSalary(int salary) {
         this.salary = salary;
+    }
+
+    public void addEmployee(String surname, String name, String patronymic, int department, int salary, int id) {
+        if (counter >= employees.length) {
+            System.out.println("Нельзя добавить сотрудника, закончилось место");
+        }
+        Employee newEmployee = new Employee();
+        employees[counter++] = newEmployee;
+//        this.counter = counter + 1;
+        this.id = this.counter;
+//        System.out.println(counter);
+//        System.out.println(this.id);
+//        System.out.println(this.name);
     }
 
     @Override
     public String toString() {
         return "Employee{" +
-                "surname='" + surname + '\'' +
+                "surname='" + getSurname() + '\'' +
                 ", name='" + name + '\'' +
                 ", patronymic='" + patronymic + '\'' +
                 ", department=" + department +
-                ", salary=" + salary +
-                ", id=" + id +
+                ", salary=" + this.salary +
                 ", counter=" + counter +
-                '}';
+                ", id=" + id +
+                 '}';
     }
 
-    public static void printAllEmployees() {
-        for (int i = 0; i < 3; i++) {
+    public void printAllEmployee() {
+        for (int i = 0; i < counter; i++) {
         Employee employee = employees[i];
-            System.out.println(employee.getName() + employee.getSurname());
+           System.out.println(employee);
         }
 
     }
--- src/Coursework/coursework.java
+++ src/Coursework/coursework.java
@@ -3,11 +3,11 @@ package Coursework;
 public class coursework {
     public static void main (String[] args) {
         Employee employee = new Employee();
-//        Employee employee1 = new Employee("Иванов", "Иван", "Иванович", 1, 10000, 1)
-        employee.addEmployee("Иванов", "Иван", "Иванович", 1, 10000, 1);
-//        employee.addEmployee("Петров", "Иван", "Иванович", 1, 10000, 2);
-//        employee.addEmployee("Сидоров", "Иван", "Иванович", 1, 10000, 3);
-//        Employee.printAllEmployees();
+        employee.addEmployee("Иванов", "Иван", "Иванович", 1, 10000, 0);
+        employee.addEmployee("Петров", "Петр", "Петрович", 2, 20000, 0 );
+        employee.addEmployee("Сидоров", "Сидор", "Сидорович", 3, 30000, 0);
+        employee.addEmployee("Васильев", "Василий", "Иванович", 4, 40000, 0);
+        employee.printAllEmployee();
         System.out.println(employee.getName());
         System.out.println(employee);
     }
